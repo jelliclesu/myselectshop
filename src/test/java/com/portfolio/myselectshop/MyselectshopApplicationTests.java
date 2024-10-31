0246eb2114f595de95cfd20f3f1a0c462d370491
jelliclesu <jelliclesu@naver.com>
2024-10-31 14:22:32 +0900

    init commit

---
+++ src/test/java/com/portfolio/myselectshop/MyselectshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.myselectshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyselectshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
