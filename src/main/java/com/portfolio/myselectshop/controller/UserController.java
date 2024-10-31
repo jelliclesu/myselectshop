4e6ba9d29ab0d74d336361e3c4314127ef6f816f
jelliclesu <jelliclesu@naver.com>
2024-10-31 22:57:20 +0900

    feat: 로그인, 회원가입 페이지 이동 기능 구현

---
+++ src/main/java/com/portfolio/myselectshop/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.portfolio.myselectshop.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserController {
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+}
