8c60c1c18761b70c499ca8c1b27eb244934e2542
jelliclesu <jelliclesu@naver.com>
2024-11-01 18:51:10 +0900

    feat: JWT 인증 방식을 위해 회원 관련 정보 받기 기능 구현

--- src/main/java/com/portfolio/myselectshop/controller/UserController.java
+++ src/main/java/com/portfolio/myselectshop/controller/UserController.java
@@ -1,16 +1,21 @@
 package com.portfolio.myselectshop.controller;
 
 import com.portfolio.myselectshop.dto.SignupRequestDto;
+import com.portfolio.myselectshop.dto.UserInfoDto;
+import com.portfolio.myselectshop.entity.UserRoleEnum;
+import com.portfolio.myselectshop.security.UserDetailsImpl;
 import com.portfolio.myselectshop.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -47,6 +52,17 @@ public class UserController {
         userService.signup(requestDto);
 
         return "redirect:/api/user/login-page";
+    }
+
+    // 회원 관련 정보 받기
+    @GetMapping("/api/user-info")
+    @ResponseBody
+    public UserInfoDto getUserInfo(UserDetailsImpl userDetails) {
+        String username = userDetails.getUser().getUsername();
+        UserRoleEnum role = userDetails.getUser().getRole();
+        boolean isAdmin = (role == UserRoleEnum.ADMIN);
+
+        return new UserInfoDto(username, isAdmin);
     }
 
 }
---
+++ src/main/java/com/portfolio/myselectshop/dto/UserInfoDto.java
@@ -0,0 +1,12 @@
+package com.portfolio.myselectshop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+public class UserInfoDto {
+    String username;
+    boolean isAdmin;
+}
