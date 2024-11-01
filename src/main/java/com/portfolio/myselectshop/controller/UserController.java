37c603bcd0634af683f065747fda9b40e92b1aa0
jelliclesu <jelliclesu@naver.com>
2024-11-01 18:20:08 +0900

    feat: 회원가입 기능 구현

--- src/main/java/com/portfolio/myselectshop/controller/UserController.java
+++ src/main/java/com/portfolio/myselectshop/controller/UserController.java
@@ -1,16 +1,26 @@
 package com.portfolio.myselectshop.controller;
 
+import com.portfolio.myselectshop.dto.SignupRequestDto;
+import com.portfolio.myselectshop.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping("/user/login-page")
     public String loginPage() {
@@ -21,4 +31,22 @@ public class UserController {
     public String signupPage() {
         return "signup";
     }
+
+    // 회원가입
+    @PostMapping("/user/signup")
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외 처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if (fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
+
+        userService.signup(requestDto);
+
+        return "redirect:/api/user/login-page";
+    }
+
 }
---
+++ src/main/java/com/portfolio/myselectshop/service/UserService.java
@@ -0,0 +1,53 @@
+package com.portfolio.myselectshop.service;
+
+import com.portfolio.myselectshop.dto.SignupRequestDto;
+import com.portfolio.myselectshop.entity.User;
+import com.portfolio.myselectshop.entity.UserRoleEnum;
+import com.portfolio.myselectshop.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    // ADMIN_TOKEN
+    public final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
+
+    public void signup(SignupRequestDto requestDto) {
+        String username = requestDto.getUsername();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+        String email = requestDto.getEmail();
+
+        // 회원 중복 확인
+        Optional<User> checkUsername = userRepository.findByUsername(username);
+        if (checkUsername.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+        }
+
+        // email 중복 확인
+        Optional<User> checkEmail = userRepository.findByEmail(email);
+        if (checkEmail.isPresent()) {
+            throw new IllegalArgumentException("중복된 Email 입니다.");
+        }
+
+        // 사용자 Role 확인
+        UserRoleEnum role = UserRoleEnum.USER;
+        if (requestDto.isAdmin()) {
+            if (!ADMIN_TOKEN.equals(requestDto.getAdminToken())) {
+                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
+            }
+            role = UserRoleEnum.ADMIN;
+        }
+
+        // 사용자 등록
+        User user = new User(username, password, email, role);
+        userRepository.save(user);
+    }
+}
