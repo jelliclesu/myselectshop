4fb1aae05dd76b11d6e1a86a3b6baa171b14ece2
jelliclesu <jelliclesu@naver.com>
2024-11-04 18:29:29 +0900

    feat: Product 와 User 의 연관관계 생성

--- src/main/java/com/portfolio/myselectshop/entity/Product.java
+++ src/main/java/com/portfolio/myselectshop/entity/Product.java
@@ -35,7 +35,7 @@ public class Product extends Timestamped {
     private int myprice;
 
 
-    public Product(ProductRequestDto requestDto) {
+    public Product(ProductRequestDto requestDto, User user) {
         this.title = requestDto.getTitle();
         this.link = requestDto.getLink();
         this.image = requestDto.getImage();
@@ -49,4 +49,8 @@ public class Product extends Timestamped {
     public void updateByItemDto(ItemDto itemDto) {
         this.lprice = itemDto.getLprice();
     }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
